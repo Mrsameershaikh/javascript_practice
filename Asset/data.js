5de9958f88ed48ef0b674d43e7579832ba404057
Sameer Shaikh <98098376+Mrsameershaikh@users.noreply.github.com>
2023-10-10 18:18:50 +0530

    accordion static data test is done

--- Asset/data.js
+++ Asset/data.js
@@ -9,3 +9,20 @@ const category=[
     "Array",
     "Object"
 ]
+
+// Array Questions & Solution
+
+const arrayData=[
+    {
+        'Q':'Create a JavaScript function that verifies whether a given input is an array or not?',
+        'Ans':'dkjfds skdjfsd sjdfds jsdfds jdkfsd'
+    },
+    {
+        'Q':'Create a JavaScript dfdsfdsfsdsfsfds or not?',
+        'Ans':'dkjfds skdjfsd sjdfds jsdfds jdkfsd'
+    },
+    {
+        'Q':'Create a JavaScript function that verifies whether a given input is an array or not?',
+        'Ans':'dkjfds skdjfsd sjdfds jsdfds jdkfsd'
+    },
+]
