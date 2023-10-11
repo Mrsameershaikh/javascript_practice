14cf7bfbc4276cbbf10f967e1482099db4048bae
Sameer Shaikh <98098376+Mrsameershaikh@users.noreply.github.com>
2023-10-11 12:13:27 +0530

    dynamic data added in all files

--- Asset/data.js
+++ Asset/data.js
@@ -1,28 +1,88 @@
 //Category
 
-const category=[
-    "String",
-    "Number",
-    "Boolean",
-    "Undefined",
-    "Null",
-    "Array",
-    "Object"
-]
+const category = [
+  "String",
+  "Number",
+  "Boolean",
+  "Undefined",
+  "Null",
+  "Array",
+  "Object",
+];
 
-// Array Questions & Solution
+const stringData = [
+  {
+    Q: "String one?",
+    Ans: "1st solution",
+  },
+  {
+    Q: "String two?",
+    Ans: "2nd solution fur dkfshd shdsgyr hdsdjf jhafksd hsgksf",
+  },
+];
 
-const arrayData=[
-    {
-        'Q':'Create a JavaScript function that verifies whether a given input is an array or not?',
-        'Ans':'1st solution'
-    },
-    {
-        'Q':'Create a JavaScript dfdsfdsfsdsfsfds or not?',
-        'Ans':'2nd solution fur dkfshd shdsgyr hdsdjf jhafksd hsgksf'
-    },
-    {
-        'Q':'Create a JavaScript function that verifies whether a given input is an array or not?',
-        'Ans':'3rd solution'
-    },
-]
+const numberData = [
+  {
+    Q: "Number one?",
+    Ans: "1st solution",
+  },
+  {
+    Q: "Number two?",
+    Ans: "2nd solution fur dkfshd shdsgyr hdsdjf jhafksd hsgksf",
+  },
+];
+
+const nullData = [
+  {
+    Q: "Null one?",
+    Ans: "1st solution",
+  },
+  {
+    Q: "Null two?",
+    Ans: "2nd solution fur dkfshd shdsgyr hdsdjf jhafksd hsgksf",
+  },
+];
+
+const undefinedData = [
+  {
+    Q: "Undefined one?",
+    Ans: "1st solution",
+  },
+  {
+    Q: "Undefined two?",
+    Ans: "2nd solution fur dkfshd shdsgyr hdsdjf jhafksd hsgksf",
+  },
+];
+
+const booleanData = [
+  {
+    Q: "Boolean one?",
+    Ans: "1st solution",
+  },
+  {
+    Q: "Boolean two?",
+    Ans: "2nd solution fur dkfshd shdsgyr hdsdjf jhafksd hsgksf",
+  },
+];
+
+const arrayData = [
+  {
+    Q: "Array one?",
+    Ans: "1st solution",
+  },
+  {
+    Q: "Array two?",
+    Ans: "2nd solution fur dkfshd shdsgyr hdsdjf jhafksd hsgksf",
+  },
+];
+
+const objectData = [
+  {
+    Q: "Object one?",
+    Ans: "1st solution",
+  },
+  {
+    Q: "Object two?",
+    Ans: "2nd solution fur dkfshd shdsgyr hdsdjf jhafksd hsgksf",
+  },
+];
